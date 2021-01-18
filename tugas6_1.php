ad8653f12235d3171035f239c381a575033713f9
troy213 <49593922+troy213@users.noreply.github.com>
2021-01-19 02:26:01 +0700

    Add files via upload

---
+++ tugas6_1.php
@@ -0,0 +1,11 @@
+<?php
+  $habib = 21;
+  $aji = $habib + 4;
+  $bintan = $aji + 3;
+
+  echo "========================================= <br>
+  Usia Habib = $habib <br>
+  Usia Aji = $aji <br>
+  Usia Bintan = $bintan <br>
+  =========================================";
+?>
---
+++ tugas6_2.php
@@ -0,0 +1,11 @@
+<?php
+  $dus = 6;
+  $jambu = $dus * 5;
+  $jual = ($jambu * 15000) + ($dus * 2000);
+
+  echo "========================================= <br>
+  Harga Jambu = Rp 15000 / Kg <br>
+  Harga Kardus = Rp 2000 / pcs <br>
+  Total Penjualan (Dus dan Jambu) = Rp $jual <br>
+  =========================================";
+?>
